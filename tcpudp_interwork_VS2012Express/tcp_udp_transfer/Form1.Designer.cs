0cc1bbde512194f90f6f7962e1b2ab3a270be41b
gisaki <gisaki@gmail.com>
2017-11-01 22:27:57 +0900

    Initial commit
    First, simply supported only One UDP to One UDP transfer

---
+++ tcpudp_interwork_VS2012Express/tcp_udp_transfer/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace tcp_udp_transfer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxBindPort = new System.Windows.Forms.TextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxSendPort = new System.Windows.Forms.TextBox();
+            this.textBoxSendIPAddress = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxBindPort
+            // 
+            this.textBoxBindPort.Location = new System.Drawing.Point(162, 27);
+            this.textBoxBindPort.Name = "textBoxBindPort";
+            this.textBoxBindPort.Size = new System.Drawing.Size(61, 19);
+            this.textBoxBindPort.TabIndex = 2;
+            this.textBoxBindPort.Text = "51111";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(278, 154);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(128, 23);
+            this.buttonStart.TabIndex = 5;
+            this.buttonStart.Text = "Start UDP Listen";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(160, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Port";
+            // 
+            // label6
+            // 
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label6.Location = new System.Drawing.Point(8, 27);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(148, 19);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Any";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // textBoxSendPort
+            // 
+            this.textBoxSendPort.Location = new System.Drawing.Point(160, 30);
+            this.textBoxSendPort.Name = "textBoxSendPort";
+            this.textBoxSendPort.Size = new System.Drawing.Size(61, 19);
+            this.textBoxSendPort.TabIndex = 2;
+            this.textBoxSendPort.Text = "51111";
+            // 
+            // textBoxSendIPAddress
+            // 
+            this.textBoxSendIPAddress.Location = new System.Drawing.Point(8, 30);
+            this.textBoxSendIPAddress.Name = "textBoxSendIPAddress";
+            this.textBoxSendIPAddress.Size = new System.Drawing.Size(146, 19);
+            this.textBoxSendIPAddress.TabIndex = 2;
+            this.textBoxSendIPAddress.Text = "192.168.123.2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "IP address";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 12);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "IP address";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(160, 15);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(26, 12);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Port";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.textBoxBindPort);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(258, 60);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Bind";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.textBoxSendIPAddress);
+            this.groupBox2.Controls.Add(this.textBoxSendPort);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Location = new System.Drawing.Point(12, 117);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(258, 60);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Send";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(66, 78);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(132, 33);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 189);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxBindPort;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxSendPort;
+        private System.Windows.Forms.TextBox textBoxSendIPAddress;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ tcpudp_interwork_VS2012Express/tcp_udp_transfer/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tcp_udp_transfer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // 
+        // フォームの表示関連
+        // 
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 三角形
+            {
+                //描画先とするImageオブジェクトを作成する
+                Bitmap canvas = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                //ImageオブジェクトのGraphicsオブジェクトを作成する
+                Graphics g = Graphics.FromImage(canvas);
+
+                //直線で接続する点の配列を作成
+                int padding = 2;
+                Point[] ps = {new Point(0 + padding, 0 + padding),
+                 new Point(pictureBox1.Width - padding, 0 + padding),
+                 new Point(pictureBox1.Width / 2, pictureBox1.Height - padding),
+                 new Point(0 + padding, 0 + padding)};
+                //多角形を描画する
+                g.FillPolygon(Brushes.Pink, ps, FillMode.Winding);
+
+                //リソースを解放する
+                g.Dispose();
+
+                //PictureBox1に表示する
+                pictureBox1.Image = canvas;
+            }
+        }
+
+        private System.Net.Sockets.UdpClient udpClient_ = null;
+//        private IPAddress sourceIPAddress_;
+        private int sourcePort_;
+
+        private IPAddress sendIPAddress_;
+        private int sendPort_;
+
+        private byte[] lastRcvBytes_;
+        private System.Net.IPEndPoint lastRemoteEP_ = null;
+
+        // 
+        // UDP送受信関連
+        // 
+
+        // UDP受信
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            // 
+            // 実施中→終了
+            // 
+            if (udpClient_ != null)
+            {
+                udpClient_.Close();
+                udpClient_ = null;
+                // ボタン等
+                change_ui(false);
+                return;
+            }
+
+            // 
+            // 未実施→実施
+            // 
+
+            // 送信元、送信先
+            try
+            {
+                sourcePort_ = Int32.Parse(textBoxBindPort.Text);
+                sendPort_ = Int32.Parse(textBoxSendPort.Text);
+                sendIPAddress_ = IPAddress.Parse(textBoxSendIPAddress.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return; // 開始せず中断
+            }
+
+            try
+            {
+                //UdpClientを作成し、指定したポート番号にバインドする
+                System.Net.IPEndPoint localEP =
+                    new System.Net.IPEndPoint(
+                        System.Net.IPAddress.Any, //sourceIPAddress_, 
+                        Int32.Parse(textBoxBindPort.Text)
+                    );
+                udpClient_ = new System.Net.Sockets.UdpClient(localEP);
+                //非同期的なデータ受信を開始する
+                udpClient_.BeginReceive(ReceiveCallback, udpClient_);
+                // ボタン等
+                change_ui(true);
+            }
+            catch (Exception ex)
+            {
+                if (udpClient_ != null)
+                {
+                    udpClient_.Close();
+                }
+                udpClient_ = null;
+
+                MessageBox.Show(ex.Message, "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        //データを受信した時
+        private void ReceiveCallback(IAsyncResult ar)
+        {
+            System.Net.Sockets.UdpClient udp =
+                (System.Net.Sockets.UdpClient)ar.AsyncState;
+
+            //非同期受信を終了する
+            System.Net.IPEndPoint remoteEP = null;
+            byte[] rcvBytes;
+            try
+            {
+                rcvBytes = udp.EndReceive(ar, ref remoteEP);
+            }
+            catch (System.Net.Sockets.SocketException ex)
+            {
+                Console.WriteLine("受信エラー({0}/{1})",
+                    ex.Message, ex.ErrorCode);
+                return;
+            }
+            catch (ObjectDisposedException ex)
+            {
+                //すでに閉じている時は終了
+                Console.WriteLine("Socketは閉じられています。");
+                return;
+            }
+
+            // 受信情報を控えておく（送信データに載せるため）
+            // 受信データ
+            lastRcvBytes_ = new byte[rcvBytes.Length];
+            Array.Copy(rcvBytes, 0, lastRcvBytes_, 0, rcvBytes.Length);
+            // 受信元
+            lastRemoteEP_ = remoteEP;
+
+            // 転送する
+            SendUDP(sendIPAddress_, sendPort_); // 送信先に転送する場合
+
+            //再びデータ受信を開始する
+            udp.BeginReceive(ReceiveCallback, udp);
+        }
+
+        // UDP送信
+        private void SendUDP(IPAddress remoteIPAddress, int remotePort)
+        {
+            //UdpClientオブジェクトを作成する
+            System.Net.Sockets.UdpClient udp =
+                new System.Net.Sockets.UdpClient();
+
+            // 送信データ
+            byte[] msg = null;
+            // 送信データは受信データと同一
+            msg = new byte[lastRcvBytes_.Length];
+            Array.Copy(lastRcvBytes_, 0, msg, 0, lastRcvBytes_.Length);
+
+            // 送信元は規定しない
+            // 送信先
+            //リモートホストを指定してデータを送信する
+            udp.Send(msg, msg.Length, remoteIPAddress.ToString(), remotePort);
+
+            //UdpClientを閉じる
+            udp.Close();
+        }
+
+        public void change_ui(Boolean start)
+        {
+            if (start)
+            {
+                buttonStart.Text = "Abort UDP Listen";
+                textBoxBindPort.Enabled = false;
+                textBoxSendIPAddress.Enabled = false;
+                textBoxSendPort.Enabled = false;
+            }
+            else
+            {
+                buttonStart.Text = "Start UDP Listen";
+                textBoxBindPort.Enabled = true;
+                textBoxSendIPAddress.Enabled = true;
+                textBoxSendPort.Enabled = true;
+            }
+        }
+
+    }
+}
